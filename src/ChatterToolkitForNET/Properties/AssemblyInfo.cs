895b6ff75d3af817e6b1767f1350c39bc48405d1
Mark Adamson <mark@adamsonfamily.co.uk>
2018-08-24 08:03:55 +0200

    Revert "Convert to SDK project style"
    This reverts commit 3120a86754d0787f03f667e573c6aba6d7f93cbb.

--- src/ChatterToolkitForNET/Properties/AssemblyInfo.cs
+++ src/ChatterToolkitForNET/Properties/AssemblyInfo.cs
@@ -1,26 +1,20 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Salesforce.Force.UnitTests")]
+[assembly: AssemblyTitle("Salesforce.Chatter")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Salesforce.Force.UnitTests")]
+[assembly: AssemblyCompany("salesforce.com")]
+[assembly: AssemblyProduct("Salesforce.Chatter")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ac3ad431-27eb-45af-a184-42ba244a5f88")]
+[assembly: NeutralResourcesLanguage("en")]
 
 // Version information for an assembly consists of the following four values:
 //
--- src/CommonLibrariesForNET/Properties/AssemblyInfo.cs
+++ src/CommonLibrariesForNET/Properties/AssemblyInfo.cs
@@ -1,26 +1,18 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Salesforce.Force.UnitTests")]
+[assembly: AssemblyTitle("Salesforce.Common")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Salesforce.Force.UnitTests")]
+[assembly: AssemblyCompany("salesforce.com")]
+[assembly: AssemblyProduct("Salesforce.Common")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ac3ad431-27eb-45af-a184-42ba244a5f88")]
+[assembly: NeutralResourcesLanguage("en")]
 
 // Version information for an assembly consists of the following four values:
 //
